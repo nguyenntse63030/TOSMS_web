bfe126f88e4ecc3e1e0020acf8c2a3fad0103a7e
nguyennt3498 <nguyentrungnguyen3498@gmail.com>
2020-05-30 11:12:14 +0700

    Add libary material bootstrap.

---
+++ public/javascripts/apiService.js
@@ -0,0 +1,6 @@
+const apiVersion = '/api/v1'
+angular.module('TOSMS').factory('apiService', ['$http', function ($http) {
+    return {
+        
+    }
+}])
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Login' });
+})
 
+router.get('/notification', function (req, res, next) {
+  res.render('notification/list', { title: 'Notification' })
+})
 module.exports = router;
