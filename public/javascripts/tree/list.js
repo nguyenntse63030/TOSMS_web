f2674f09020a1b4859cb05edba12a5dbb6c2784c
quang111 <ooosurikenooo@gmail.com>
2020-05-31 09:00:31 +0700

    triển khai giao diện trang danh sách cây

---
+++ public/javascripts/tree/list.js
@@ -0,0 +1,44 @@
+app.controller('listController', ['$scope', 'apiService', function ($scope, apiService) {
+    let options = {
+        language: {
+            decimal: '.',
+            thousands: ',',
+            url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Vietnamese.json'
+        },
+        ajax: {
+            url: '/javascripts/tree/list.json',
+            dataSrc: (response) => {
+               return response.map((tree) => {
+                    return {
+                        createdTime: generateATag(tree, 'createdTime'),
+                        id: generateATag(tree, 'id'),
+                        img: generateATag(tree, 'img'),
+                        location: generateATag(tree, 'location'),
+                        status: generateATag(tree, 'status'),
+                    }
+               })
+            }
+        },
+        columns: [
+            { data: 'createdTime' },
+            { data: 'id' },
+            { data: 'img' },
+            { data: 'location' },
+            { data: 'status' }
+        ]
+    }
+    $('#tree-table').DataTable(options);
+}])
+
+function generateATag(tree, property) {
+    let data = tree[property]
+    if (property === 'createdTime') {
+        data = parseInt(data)
+        data = formatDate(data)
+    } else if (property === 'img') {
+        return '<a class="table-row-link" href="/tree/' + tree.id + '"><img class="img-data-row" alt="treeImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQbQa3ZRo97YeHzyY7kBJPtS2nQx0u5dcMjD8rm2yLphWgWE3ci&usqp=CAU"/></a>'
+    } 
+
+    let result = '<a class="table-row-link" href="/tree/' + tree.id + '">' + data + '</a>'
+    return result
+}
--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,10 @@ router.get('/employee/:id', function (req, res, next) {
   res.render('employee/detail', { title: 'Hồ Sơ', code: '' })
 })
 
+router.get('/tree', function (req, res, next) {
+  res.render('tree/list', { title: 'Cây' })
+})
+
 router.get('/notification', function (req, res, next) {
   res.render('notification/list', { title: 'Notification' })
 })
