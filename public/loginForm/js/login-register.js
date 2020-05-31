c23c5f39aba002a4e6d3244bd5cd723c7f6de7d3
tungapolo <tungapolo123@gmail.com>
2020-05-31 23:00:23 +0700

    Login UI

---
+++ public/loginForm/js/login-register.js
@@ -0,0 +1,95 @@
+/*
+ *
+ * login-register modal
+ * Autor: Creative Tim
+ * Web-autor: creative.tim
+ * Web script: http://creative-tim.com
+ * 
+ */
+function showRegisterForm() {
+    $('.loginBox').fadeOut('fast', function () {
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast', function () {
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Register with');
+    });
+    $('.error').removeClass('alert alert-danger').html('');
+
+}
+function showLoginForm() {
+    $('#loginModal .registerBox').fadeOut('fast', function () {
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast', function () {
+            $('.login-footer').fadeIn('fast');
+        });
+
+        $('.modal-title').html('Login with');
+    });
+    $('.error').removeClass('alert alert-danger').html('');
+}
+
+function openLoginModal() {
+    showLoginForm();
+    setTimeout(function () {
+        $('#loginModal').modal({
+            backdrop: 'static',
+            keyboard: false,
+            show: true
+        });
+    }, 230);
+
+}
+function openRegisterModal() {
+    showRegisterForm();
+    setTimeout(function () {
+        $('#loginModal').modal('show');
+    }, 230);
+
+}
+
+function loginAjax() {
+    var $form = $('form')
+    var data = getFormData($form)
+    $.ajax({
+        url: '/api/v1/auth/sign_in',
+        type: 'POST',
+        data: data,
+        complete: function (res) {
+            if (res.status !== 200) {
+                return shakeModal(res.responseJSON.errorMessage)
+            } else {
+                COMMON.setCookie('user', JSON.stringify(res.responseJSON.user))
+                if (res.responseJSON.user.role === 'Supervisor') {
+                    window.location.href = '/user'
+                } else if (res.responseJSON.user.role === 'Manager') {
+                    window.location.href = '/user'
+                } else {
+                    return shakeModal(res.responseJSON.errorMessage)
+                }
+            }
+        }
+    })
+
+    /*   Simulate error message from the server   */
+    // shakeModal();
+}
+
+function shakeModal() {
+    $('#loginModal .modal-dialog').addClass('shake');
+    $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+    $('input[type="password"]').val('');
+    setTimeout(function () {
+        $('#loginModal .modal-dialog').removeClass('shake');
+    }, 1000);
+}
+
+function getFormData($form) {
+    var unindexedArray = $form.serializeArray()
+    var indexedArray = {}
+    $.map(unindexedArray, function (n, i) {
+        indexedArray[n['name']] = n['value'].trim()
+    })
+    return indexedArray
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Login' });
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Login" });
+});
 
-router.get('/employee', function (req, res, next) {
-  res.render('employee/list', { title: 'Nhân Viên' })
-})
+router.get("/employee", function (req, res, next) {
+  res.render("employee/list", { title: "Nhân Viên" });
+});
+router.get("/employee/create", function (req, res, next) {
+  res.render("employee/create", { title: "Tạo User" });
+});
 
-router.get('/employee/:id', function (req, res, next) {
-  res.render('employee/detail', { title: 'Hồ Sơ', code: '' })
-})
+router.get("/employee/:id", function (req, res, next) {
+  res.render("employee/detail", { title: "Hồ Sơ", code: "" });
+});
 
-router.get('/tree', function (req, res, next) {
-  res.render('tree/list', { title: 'Cây' })
-})
+router.get("/tree", function (req, res, next) {
+  res.render("tree/list", { title: "Cây" });
+});
+router.get("/tree/create", function (req, res, next) {
+  res.render("tree/create", { title: " Tạo Thông Tin Cây" });
+});
+router.get("/camera/create", function (req, res, next) {
+  res.render("camera/create", { title: "Tạo Thông Tin Camera" });
+});
 
-router.get('/notification', function (req, res, next) {
-  res.render('notification/list', { title: 'Notification' })
-})
+router.get("/notification", function (req, res, next) {
+  res.render("notification/list", { title: "Notification" });
+});
 
-router.get('/notification/:id', function (req, res, next) {
-  let treeID = req.params.id
-  res.render('notification/detail', { title: 'Notification Detail', treeID: treeID })
-})
+router.get("/notification/:id", function (req, res, next) {
+  let treeID = req.params.id;
+  res.render("notification/detail", {
+    title: "Notification Detail",
+    treeID: treeID,
+  });
+});
 module.exports = router;
