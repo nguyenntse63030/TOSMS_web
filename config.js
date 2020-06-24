0ca13ff9f4c6ee6976136cde8b0dd1e686a27986
nguyennt3498 <nguyentrungnguyen3498@gmail.com>
2020-06-24 12:21:04 +0700

    Clear cache file config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  AWS: {
-    ACCESS_KEY_ID: "AKIAJQVWO3JDFAJD44KA",
-    SECRETT_KEY: "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",
-    BUCKET_NAME: "nodejs-system",
-    SES_REGION: "ap-southeast-2",
-  },
-};
