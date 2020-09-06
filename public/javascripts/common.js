6cf72e9be3f00c500a25ca11390dcc488b353508
quang111 <ooosurikenooo@gmail.com>
2020-09-06 08:56:12 +0700

    hàm check format email

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -203,6 +203,11 @@ function validateCreateTree(file, treeType, street) {
   return check;
 }
 
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
 function loadLocation($scope, apiService, change) {
   $scope.getListCities = () => {
     apiService
